79ee99d26948c1757490f8dbc69b22de7919b039
Ilya Belyansky <i.belyansky@laraue.com>
2026-09-16 12:58:18 +0700

    feat: add cancel state token rservations job

---
+++ src/Laraue.Apps.Billing.DataAccess/Migrations/20260916032017_AddJobStates.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Laraue.Apps.Billing.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddJobStates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "job_states",
+                columns: table => new
+                {
+                    job_name = table.Column<string>(type: "text", nullable: false),
+                    job_data = table.Column<string>(type: "text", nullable: true),
+                    last_execution_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    next_execution_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_job_states", x => x.job_name);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "job_states");
+        }
+    }
+}
